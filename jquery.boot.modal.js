fdb842b7001b326c4b69e3741a2296f1202151bb
daegyu jeong <babypaunch@gmail.com>
2017-03-23 11:03:42 +0900

    대대적인 코드 수정
    코드 리팩토링

--- jquery.boot.modal.js
+++ jquery.boot.modal.js
@@ -6,58 +6,83 @@ function cssToStyle(json){
 	return result;
 }
 
-function ACE(type, text, callback){
-	/*
-	* type에 따라 alert/confirm/error로 분류한다.
-	* width는 modal의 전체 크기를 pixel 값으로 지정함.
-	* static이 true면 배경을 클릭해도 modal이 닫히지 않는다.
-	* footer는 보이지 않게 처리하고자 하는 경우, 공백을 대입하면 된다.
-	*/
-	var defaults = {
-		type: type
-		, id: "ace-modal-" + type //modal id
-		, width: 400
-		, static: true
-		, header: {
-			title: type.toUpperCase()
-			, closeButton: true
-			, css: {
-				"border-top-left-radius": "5px"
-				, "border-top-right-radius": "5px"
-				, background: type === "alert" ? "orange" : type === "confirm" ? "#3ae" : "red"
-				, color: type === "error" ? "white" : "black"
-			}
+var BootModal = {
+	defaults: function(){
+		console.log(arguments);
+		if(arguments.length === 1){ //button modal
+			return {
+				id: "" //modal id
+				, width: 1000
+				, static: true
+				, header: {
+					title: ""
+					, closeButton: true
+					, css: {}
+				}
+				, body: {
+					css: {
+						overflow: "auto"
+					}
+					, html: ""
+				}
+				, footer: "<a href='#' data-dismiss='modal' class='btn btn-default'>Cancel</a> <a href='#' class='btn btn-success'>Save</a>"
+			};
+		}else{ //direct modal
+			/*
+			* type에 따라 alert/confirm/error로 분류한다.
+			* width는 modal의 전체 크기를 pixel 값으로 지정함.
+			* static이 true면 배경을 클릭해도 modal이 닫히지 않는다.
+			* footer는 보이지 않게 처리하고자 하는 경우, 공백을 대입하면 된다.
+			*/
+			return {
+				type: arguments[0]
+				, id: "ace-modal-" + arguments[0] //modal id
+				, width: 400
+				, static: true
+				, header: {
+					title: arguments[0].toUpperCase()
+					, closeButton: true
+					, css: {
+						"border-top-left-radius": "5px"
+						, "border-top-right-radius": "5px"
+						, background: arguments[0] === "alert" ? "orange" : arguments[0] === "confirm" ? "#3ae" : "red"
+						, color: arguments[0] === "error" ? "white" : "black"
+					}
+				}
+				, body: {
+					css: {
+						overflow: "auto"
+					}
+					, html: arguments[1]
+				}
+				, footer: "<a href='#' data-dismiss='modal' class='btn btn-default'>" + (arguments[0] === "confirm" ? "Cancel" : "Close") + "</a>" + (arguments[0] === "confirm" ? " <a href='#' data-dismiss='modal' class='btn btn-success'>Agree</a>" : "")
+			};
 		}
-		, body: {
-			css: {
-				overflow: "auto"
-			}
-			, html: text
-		}
-		, footer: "<a href='#' data-dismiss='modal' class='btn btn-default'>" + (type === "confirm" ? "Cancel" : "Close") + "</a>" + (type === "confirm" ? " <a href='#' data-dismiss='modal' class='btn btn-success'>Agree</a>" : "")
-	};
-
-	var modal = (function(defaults){
+	} //end: defaults: function(){
+	, inner: function(defaults){
 		return ""
-		+ "<div class='modal' id='" + defaults.id + "' " + (defaults.static ? "data-backdrop='static'" : "") + ">\n"
-			+ "<div style='display:table; height: 100%; width: 100%;'>\n"
-				+ "<div class='modal-dialog' style='width: " + defaults.width + "px; display: table-cell; vertical-align: middle;'>\n"
-					+ "<div class='modal-content' style='width: inherit; height: inherit; margin: 0 auto;'>\n"
-						+ "<div class='modal-header' style='" + cssToStyle(defaults.header.css) + "'>\n"
-							+ (defaults.header.closeButton ? "<button type='button' class='close' data-dismiss='modal'>×</button>\n" : "")
-							+ "<h4 class='modal-title'>" + defaults.header.title + "</h4>\n"
-						+ "</div>\n"
-						+ "<div class='modal-body' style='" + cssToStyle(defaults.body.css) + "'>\n"
-							+ defaults.body.html
-						+ "</div>\n"
-						+ (defaults.footer === "" ? "" : "<div class='modal-footer' style='text-align: center;'>" + defaults.footer + "</div>\n")
+		+ "<div style='display:table; height: 100%; width: 100%;'>\n"
+			+ "<div class='modal-dialog' style='width: " + defaults.width + "px; display: table-cell; vertical-align: middle;'>\n"
+				+ "<div class='modal-content' style='width: inherit; height: inherit; margin: 0 auto;'>\n"
+					+ "<div class='modal-header' style='" + cssToStyle(defaults.header.css) + "'>\n"
+						+ (defaults.header.closeButton ? "<button type='button' class='close' data-dismiss='modal'>×</button>\n" : "")
+						+ "<h4 class='modal-title'>" + defaults.header.title + "</h4>\n"
 					+ "</div>\n"
+					+ "<div class='modal-body' style='" + cssToStyle(defaults.body.css) + "'>\n"
+						+ defaults.body.html
+					+ "</div>\n"
+					+ (defaults.footer === "" ? "" : "<div class='modal-footer' style='text-align: center;'>" + defaults.footer + "</div>\n")
 				+ "</div>\n"
 			+ "</div>\n"
 		+ "</div>\n";
-	})(defaults);
+	} //end: , inner: function(defaults){
+}; //end: var BootModal = {
 
+function ACE(type, text, callback){
+	var defaults = BootModal.defaults(arguments[0], arguments[1], arguments[2]);
+	var modal = "<div class='modal' id='" + defaults.id + "' " + (defaults.static ? "data-backdrop='static'" : "") + ">\n" + BootModal.inner(defaults) + "</div>\n";
 	var $ace = $("body #ace-modal-" + type);
+
 	if($ace.length !== 0){
 		$ace.remove();
 	}
@@ -85,55 +110,33 @@ function CONFIRM(text, callback){
 function ERROR(text){
 	new ACE("error", text);
 }
-
-$.fn.modal = function(data){
-	/*
-	* width는 modal의 전체 크기를 pixel 값으로 지정함.
-	* static이 true면 배경을 클릭해도 modal이 닫히지 않는다.
-	* footer는 보이지 않게 처리하고자 하는 경우, 공백을 대입하면 된다.
-	*/
-	var defaults = {
-		id: "" //modal id
-		, width: 1000
-		, static: true
-		, header: {
-			title: ""
-			, closeButton: true
-			, css: {}
-		}
-		, body: {
-			css: {
-				overflow: "auto"
-			}
-			, html: ""
-		}
-		, footer: "<a href='#' data-dismiss='modal' class='btn btn-default'>Cancel</a> <a href='#' class='btn btn-success'>Save</a>"
-	};
+function _MODAL(data){
+	var defaults = BootModal.defaults(data);
 	$.extend(true, defaults, data);
 
-	var modal = (function(defaults){
-		return ""
-		+ "<div class='modal' id='" + defaults.id + "' " + (defaults.static ? "data-backdrop='static'" : "") + ">"
-			+ "<div style='display:table; height: 100%; width: 100%;'>"
-				+ "<div class='modal-dialog' style='width: " + defaults.width + "px; display: table-cell; vertical-align: middle;'>"
-					+ "<div class='modal-content' style='width: inherit; height: inherit; margin: 0 auto;'>"
-						+ "<div class='modal-header' style='" + cssToStyle(defaults.header.css) + "'>"
-							+ (defaults.header.closeButton ? "<button type='button' class='close' data-dismiss='modal'>×</button>" : "")
-							+ "<h4 class='modal-title'>"
-								+ defaults.header.title
-							+ "</h4>"
-						+ "</div>"
-						+ "<div class='modal-body' style='" + cssToStyle(defaults.body.css) + "'>"
-							+ defaults.body.html
-						+ "</div>"
-						+ (defaults.footer === "" ? "" : "<div class='modal-footer' style='text-align: center;'>" + defaults.footer + "</div>")
-					+ "</div>"
-				+ "</div>"
-			+ "</div>"
-		+ "</div>";
-	})(defaults);
+	var $modal = $("#" + defaults.id);
+	var inner = BootModal.inner(defaults);
+	var modal = $modal.length === 0 ? "<div class='modal' id='" + defaults.id + "' " + (defaults.static ? "data-backdrop='static'" : "") + ">" + inner + "</div>\n" : inner;
 
-	$("body").append(modal);
+	$modal.length === 0 ? $("body").append(modal) : $modal.html(modal);
+
+	return defaults;
+}
+function MODAL(data){
+	var defaults = _MODAL(data);
+	var $modal = $("#" + defaults.id);
+
+	$modal.show();
+	$("body").append('<div class="modal-backdrop in"></div>');
+
+	$("#" + defaults.id  + " [data-dismiss]").click(function(){
+		$modal.hide();
+		$("div.modal-backdrop.in").remove();
+	});
+}
+
+$.fn.modal = function(data){
+	var defaults = _MODAL(data);
 
 	return this.each(function(){
 		$(this).attr({"data-toggle": "modal", "href": "#" + defaults.id});
